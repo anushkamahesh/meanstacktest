fba2f20a969ac11191e12824c57f338230168b22
anushkamahesh <anushkamahesh@bitbucket.org>
2016-02-14 12:43:20 +0530

    adding layout

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var app = express();
 
 app.set('views', __dirname+'/server/views');
-app.set('view_engine', 'jade');
+app.set('view engine', 'jade');
 
 app.get('*', function(req,res){
-   res.redner('index');
+   res.render('index');
 });
+
+var port = 3030;
+
+app.listen(port);
+
+console.log('Listening to port '+port+' ....');
